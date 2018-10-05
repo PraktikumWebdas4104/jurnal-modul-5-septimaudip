af2f1f6c93cf22f2aeb9c3886b8b9c7777aef4d5
Septi Maudi Pratiwi <43517394+septimaudip@users.noreply.github.com>
2018-10-05 10:22:16 +0700

    komentar.php
    **************************
    SEPTI MAUDI PRATIWI
    6701174044
    D3MI41-04
    ***************************

--- komentar.php
+++ komentar.php
@@ -1,3 +1,20 @@
-<?php
+<h1>APLIKASI MENGHITUNG JUMLAH KATA</h1>
+<form method="POST">
+	KOMENTAR : <br> <textarea name="komentar"></textarea><br><br>
+	<input type="submit" name="submit" value="submit">
+</form>
 
-?>
+<?php 
+	if (isset($_POST['submit'])) {
+		$komentar=$_POST['komentar'];
+		if (str_word_count($komentar)>=5) {
+			echo "$komentar";
+		}else{
+			echo "Komentar harus lebih dari 5 kata!";
+		}
+		
+	}
+
+
+
+ ?>
