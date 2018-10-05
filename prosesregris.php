333be183fb42aa2eb56676e4c74752214b12f1ea
Septi Maudi Pratiwi <43517394+septimaudip@users.noreply.github.com>
2018-10-05 10:26:06 +0700

    prosesregris.php
    **********************************
    SEPTI MAUDI PRATIWI
    6701174044
    D3MI41-04
    ****************************

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,38 @@
 <?php
+	$koneksi = new mysqli("localhost", "root", "", "mahasiswa");
 
+  			if (isset($_POST['submit'])) {
+				$koneksi = new mysqli("localhost", "root", "", "mahasiswa");
 
-?>
+				if ($koneksi) {
+					$nim = $_POST['nim'];
+					$nama = $_POST['nama'];
+					$email = $_POST['email'];
+
+					if (strlen($nim)==10 && is_numeric($nim)==TRUE) {
+						if (strlen($nama)<=25 && is_string($nama)==TRUE) {
+							if (strpos($email, '@gmail.com')==TRUE || strpos($email, '@yahoo.com')==TRUE) {
+								$sql = $koneksi->query("
+								INSERT INTO `mahasiswabaru` (`nim`, `nama`, `email`)
+								VALUES ('$nim', '$nama', '$email')
+										   ");
+								echo "REGISTRASI BERHASIL<br>";
+							}else{
+								echo "Email Tidak Valid";
+							}
+						}else{
+							echo "Nama tidak boleh melebihi 25 karakter dan tidak boleh bertipe angka!";
+						}
+					}else{
+						echo "NIM harus 10 karakter dan bertipe angka!";
+
+					}
+					
+				}
+				else{
+					echo "REGISTRASI GAGAL";
+				}
+
+  			}
+  		
+ ?>
