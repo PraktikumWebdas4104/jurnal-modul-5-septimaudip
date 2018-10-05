d2928dfee24cb4600112a97373cd03e170b46186
Septi Maudi Pratiwi <43517394+septimaudip@users.noreply.github.com>
2018-10-05 10:19:44 +0700

    regristrasi.php
    Menginputkan nim, nama, email
    
    *************************
    SEPTI MAUDI PRATIWI
    6701174044
    D3MI41-04
    **************************

--- regristrasi.php
+++ regristrasi.php
@@ -4,8 +4,38 @@
 	<title>Form Registrasi</title>
 </head>
 <body>
-
-
-
+<form method="POST">
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="text" name="nim"></td>
+		</tr>
+		<tr>
+			<td>NAMA</td>
+			<td>:</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>EMAIL</td>
+			<td>:</td>
+			<td><input type="email" name="email"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="submit" name="submit" value="DAFTAR"></td>
+		</tr>
+	</table>
+</form>
 </body>
+
+<?php
+	if (isset($_POST['submit'])) {
+		include 'prosesregris.php';
+	}
+
+?>
+
+
 </html>
